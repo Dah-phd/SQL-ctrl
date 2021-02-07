9db83de1db3aeefd15e67a69adb1c70eefe878f3
Daniel <toolsproed@gmail.com>
2021-02-07 19:08:03 +0200

    push bug fix in output for testing

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Simple light mySQL interface to extract data from localDB,
 import data from csv, etc...
 It is TS based, using date as measures, and is capable to autofill mising data (asuming no changes has happened from previous moment).
+
+
+FOUND A BUG THE OUTPUT MODULE IS NOT SELECTING PROPERLY DATA TO FETCH IT SIMPLY SELECTS THE FIRST COLUMNS
--- moduls/output.py
+++ moduls/output.py
@@ -5,18 +5,19 @@ if __name__ == '__main__':
     import Controler
 
 
-def export_csv(self, name, list=[]):
-    if list == '':
-        names = self.freeze_tab()
+def export_csv(self, name, _list=[]):
+    base = self.freeze_tab()
+    if _list == '':
+        names = base.copy()
     else:
-        names = list.split(',')
+        names = _list.split(',')
         names.insert(0, 'date_')
     self.cursor.execute(f'SELECT * FROM {self.table}')
     data = [t for t in self.cursor]
     df = {}
-    for x, t in enumerate(names):
-        list = [t[x] for t in data]
-        df[t] = list
+    for t in names:
+        _list = [t1[base.index(t)] for t1 in data]
+        df[t] = _list
     df = pandas.DataFrame(df)
     print(df)
     if name != 'File name':
@@ -26,18 +27,19 @@ def export_csv(self, name, list=[]):
     df.to_csv(name, index=False)
 
 
-def export(self, list=[]):
-    if list == '':
-        names = self.freeze_tab()
+def export(self, _list=[]):
+    base = self.freeze_tab()
+    if _list == '':
+        names = base.copy()
     else:
-        names = list.split(',')
+        names = _list.split(',')
         names.insert(0, 'date_')
     self.cursor.execute(f'SELECT * FROM {self.table}')
     data = [t for t in self.cursor]
     df = {}
-    for x, t in enumerate(names):
-        list = [t[x] for t in data]
-        df[t] = list
+    for t in names:
+        _list = [t1[base.index(t)] for t1 in data]
+        df[t] = _list
     df = pandas.DataFrame(df)
     pandas.set_option("display.max_rows", None,
                       "display.max_columns", None)
